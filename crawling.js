7c035528934d3315e636d85cf7a3f7a3e93d983f
kyum8562@gmail.com <66922359+kyum8562@users.noreply.github.com>
2022-04-06 16:16:45 +0900

    0406

--- crawling.js
+++ crawling.js
@@ -1,7 +1,8 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const log = console.log;
-const resultList = [];
+let resultList = [];
+var title = '';
 const getHtml = async () => {
   try {
     return await axios.get("https://movie.naver.com/movie/bi/mi/basic.naver?code=190695#");
@@ -16,24 +17,21 @@ await getHtml()
     let result = {};
     const $ = cheerio.load(html.data);
     const $bodyList = $("div.mv_info_area").children("div.mv_info");
-    var title ;
-    var rating;
-    var summary;
     $bodyList.each(function(i, elem) {
           result['title']= $(this).find('h3.h_movie a').text().slice(0,-3);
           result['rating']= $(this).find('div.main_score div.score a.ntz_score div.star_score span.st_off span.st_on').text().slice(7);
           result['summary']= $(this).find('dl.info_spec dd p span a').text();
     });
-
     return result;
   })
   .then(res => {
-      resultList.push(res);
-    //   log(res)
+      log(res);
+      title = res.title;
+      JSON.stringify(resultList.push(res));
+      log(title);
     });
 }
 getData();
-console.log(resultList);
 
 
 module.exports = { getData };
